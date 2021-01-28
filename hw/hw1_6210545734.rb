90e14b0238e9c29a10bb384f4757f9e75f49164c
noboomta <puvana.jp36@gmail.com>
2021-01-28 15:46:27 +0700

    change double quotes to single

--- hw/hw1_6210545734.rb
+++ hw/hw1_6210545734.rb
@@ -4,9 +4,9 @@ student_id = 6_210_545_734
 name = 'Puvana Swatvanitht'
 
 # display
-print "Hello, my name is ", name, " and my student id is ", student_id, "\n"
-print "What is your name: "
+print 'Hello, my name is ', name, ' and my student id is ', student_id, "\n"
+print 'What is your name: '
 
 # input
 visitor_name = gets.chomp
-puts "Hello #{visitor_name}, nice to meet you."
+puts "Hello #{visitor_name}, nice to meet you.";
